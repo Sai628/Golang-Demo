064092e897c10662f5e43bdf6c17df60392a7faa
Sai628 <sai100628@gmail.com>
2017-01-03 15:23:06 +0800

    add "variables"

---
+++ 003_variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main()  {
+    var a string = "initial"
+    fmt.Println(a)  // initial
+
+    var b, c int = 1, 2
+    fmt.Println(b, c)  // 1 2
+
+    var d = true
+    fmt.Println(d)  // true
+
+    f := "short"
+    fmt.Println(f)  // short
+}
