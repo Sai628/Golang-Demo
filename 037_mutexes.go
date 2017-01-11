ebfdaad124bcf6ca8062c8fd18cfebd6f5d00631
Sai628 <sai100628@gmail.com>
2017-01-11 20:11:09 +0800

    add mutexes

---
+++ 037_mutexes.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "sync"
+    "sync/atomic"
+    "time"
+)
+
+func main() {
+
+    var state = make(map[int]int)
+    var mutex = &sync.Mutex{}
+    var readOps uint64 = 0
+    var writeOps uint64 = 0
+
+    for r := 0; r < 100; r++ {
+        go func() {
+            total := 0
+            for {
+                key := rand.Intn(5)
+
+                mutex.Lock()
+                total += state[key]
+                mutex.Unlock()
+                
+                atomic.AddUint64(&readOps, 1)
+                time.Sleep(time.Millisecond)
+            }
+        }()
+    }
+
+    for w := 0; w < 10; w++ {
+        go func() {
+            for {
+                key := rand.Intn(5)
+                val := rand.Intn(100)
+
+                mutex.Lock()
+                state[key] = val
+                mutex.Unlock()
+                
+                atomic.AddUint64(&writeOps, 1)
+                time.Sleep(time.Millisecond)
+            }
+        }()
+    }
+
+    time.Sleep(time.Second)
+
+    readOpsFinal := atomic.LoadUint64(&readOps)
+    fmt.Println("readOps:", readOpsFinal)
+    writeOpsFinal := atomic.LoadUint64(&writeOps)
+    fmt.Println("writeOps:", writeOpsFinal)
+
+    mutex.Lock()
+    fmt.Println("state:", state)
+    mutex.Unlock()
+
+    // output like the following(they are base on go runtime):
+
+    // readOps: 77413
+    // writeOps: 7750
+    // state: map[0:40 2:94 3:0 1:5 4:2]
+}
